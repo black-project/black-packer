df5a967e83086b19847699388122090b3b0d8217
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2014-05-19 15:45:18 +0200

    quickfix for composer

--- packer/support/scripts/composer.sh
+++ packer/support/scripts/composer.sh
@@ -3,9 +3,9 @@
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
-composer global require psy/psysh 0.1.*
-composer global require zerkalica/phpcs dev-master
-composer global require phpunit/phpunit 4.0.*
-composer global require phpmd/phpmd 1.5.*
+-u vagrant composer global require psy/psysh 0.1.*
+-u vagrant composer global require zerkalica/phpcs dev-master
+-u vagrant composer global require phpunit/phpunit 4.0.*
+-u vagrant composer global require phpmd/phpmd 1.5.*
 
 echo 'export PATH=$PATH:/home/vagrant/.composer/vendor/bin'  >> /home/vagrant/.profile
