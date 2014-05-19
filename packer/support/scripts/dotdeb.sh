ee78486ce54e1a780d92da225bd8f2d1f1a449c9
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2014-05-19 16:40:32 +0200

    Increase xdebug max_nesting_level

--- packer/support/scripts/dotdeb.sh
+++ packer/support/scripts/dotdeb.sh
@@ -22,6 +22,7 @@ service nginx stop
 cp /tmp/php-cli.ini /etc/php5/cli/php.ini
 cp /tmp/php-fpm.ini /etc/php5/fpm/php.ini
 cp /tmp/nginx-default /etc/nginx/sites-available/default
+cp /tmp/20-xdebug.ini /etc/php5/mods-available/xdebug.ini
 
 # Start services
 service php5-fpm start
