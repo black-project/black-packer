f31de2f8804d26f463753cc770a5ece4a45fed09
Alexandre 'pocky' Balmes <albalmes@gmail.com>
2014-05-30 01:56:24 +0200

    Add rsa

--- packer/support/scripts/vagrant.sh
+++ packer/support/scripts/vagrant.sh
@@ -11,8 +11,6 @@ useradd -G sudo -p $(perl -e'print crypt("vagrant", "vagrant")') -m -s /bin/bash
 mkdir -pm 700 /home/vagrant/.ssh
 curl -Lo /home/vagrant/.ssh/authorized_keys \
   'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
 
 # Customize the message of the day
 echo 'Welcome to your Black virtual machine.' > /var/run/motd
